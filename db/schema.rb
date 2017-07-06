bdb54bd4632d6229bb447c114a4e6fc1a71761eb
justinmcdavid <jmcdavid@gmail.comgit config --global user.email jmcdavid@gmail.com>
2017-07-06 12:30:15 -0700

    created join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170706182454) do
+ActiveRecord::Schema.define(version: 20170706192631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20170706182454) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_locations", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "location_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["location_id"], name: "index_user_locations_on_location_id"
+    t.index ["user_id"], name: "index_user_locations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.date "join_date"
     t.string "name"
@@ -37,4 +46,6 @@ ActiveRecord::Schema.define(version: 20170706182454) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "user_locations", "locations"
+  add_foreign_key "user_locations", "users"
 end
