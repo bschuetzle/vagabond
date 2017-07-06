76a54cae68d5c63c417b756d26f219aca35b0fb1
drstrangele <chickenle@gmail.com>
2017-07-06 11:54:13 -0700

    created basic location routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705233840) do
+ActiveRecord::Schema.define(version: 20170706182454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.string "city"
+    t.string "state"
+    t.string "region"
+    t.string "photo"
+    t.decimal "lat"
+    t.decimal "long"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.date "join_date"
