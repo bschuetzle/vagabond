8f8b593456f35aba5b0de2492d0a81e8d7bedd6c
bschuetzle <bill_schuetzle@sbcglobal.net>
2017-07-07 18:09:07 -0700

    fixed bug showing logged out after signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-
     if user.save
+      login(user)
       redirect_to user_path(user)
     else render :new
     end
