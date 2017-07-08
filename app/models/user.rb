4c5c802d0c7b780362c233f0c1cb4e6333246fec
bschuetzle <bill_schuetzle@sbcglobal.net>
2017-07-07 18:58:00 -0700

    photo uploads working for edit user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :posts, dependent: :destroy
+  has_attached_file :photo
+  validates_attachment :photo, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
