242f5f33ef55fe2d3191067aa960e8286191d7a9
bschuetzle <bill_schuetzle@sbcglobal.net>
2017-07-07 18:41:09 -0700

    added photo attachment cols to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170707213507) do
+ActiveRecord::Schema.define(version: 20170708013825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,10 @@ ActiveRecord::Schema.define(version: 20170707213507) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo_file_name"
+    t.string "photo_content_type"
+    t.integer "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   add_foreign_key "posts", "locations"
