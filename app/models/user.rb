a8102f8de8c0c1681a17c53e3f9b2d0abf0c2f4b
bschuetzle <bill_schuetzle@sbcglobal.net>
2017-07-10 12:14:51 -0700

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :posts, dependent: :destroy
-  has_attached_file :photo, styles: { profile: ["100x100#", :jpg] }
+  has_attached_file :photo, styles: { profile: ["100x100#", :jpg] }, default_url: ActionController::Base.helpers.asset_path("skrillicon.jpeg", :digest => false)
   validates_attachment :photo, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
     @user ? @user.authenticate(params[:password]) : false
   end
+
 end
