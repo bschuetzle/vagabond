7033dce2247c9b621f150daa9d4cc3b84f296f87
bschuetzle <bill_schuetzle@sbcglobal.net>
2017-07-09 20:01:57 -0700

    geocoding is functional

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,11 @@ class Location < ApplicationRecord
   has_many :posts
   has_attached_file :photo, styles: { original: ["1000x500#", :jpg] }
   validates_attachment :photo, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+
+  geocoded_by :address, :latitude  => :lat, :longitude => :long
+  after_validation :geocode
+
+  def address
+    [city, state, region].compact.join(', ')
+  end
 end
