848930770ecd66406d55a90c41c76c8cfc931f90
bschuetzle <bill_schuetzle@sbcglobal.net>
2017-07-11 09:59:39 -0700

    resized skrillicon

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :posts, dependent: :destroy
   # has_attached_file :photo, styles: { profile: ["100x100#", :jpg] }, default_url: ActionController::Base.helpers.asset_path("skrillicon.jpeg", :digest => false)
-  has_attached_file :photo, styles: { profile: ["150x150#", :jpeg] }, default_url: "/images/:style/skrillicon.png"
+  has_attached_file :photo, styles: { profile: ["150x150#", :jpeg] }, default_url: "/images/:style/skrillicon.jpg"
   validates_attachment :photo, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
